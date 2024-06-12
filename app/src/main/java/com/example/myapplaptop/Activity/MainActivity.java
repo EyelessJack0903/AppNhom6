d5345160345090f99405b645b34b97b758a85ece
Luong <2254810040@vaa.edu.vn>
2024-06-13 00:38:12 +0700

    Luong update them signup login voi intro

--- app/src/main/java/com/example/myapplaptop/Activity/MainActivity.java
+++ app/src/main/java/com/example/myapplaptop/Activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplaptop;
+package com.example.myapplaptop.Activity;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapplaptop.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/myapplaptop/Activity/SignupActivity.java
@@ -0,0 +1,55 @@
+package com.example.myapplaptop.Activity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapplaptop.R;
+import com.example.myapplaptop.databinding.ActivitySignupBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+
+public class SignupActivity extends BaseActivity {
+    ActivitySignupBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivitySignupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setVariable();
+    }
+
+    private void setVariable(){
+        binding.SignUpBtn.setOnClickListener(v -> {
+            String email=binding.userEdit.getText().toString();
+            String password=binding.passEdit.getText().toString();
+
+            if(password.length()<6) {
+                Toast.makeText(SignupActivity.this, "your password must be 6 character", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(SignupActivity.this, task -> {
+                if(task.isComplete()){
+                    Log.i(TAG, "onComplete: ");
+                    startActivity(new Intent(SignupActivity.this, MainActivity.class));
+                }else {
+                    Log.i(TAG, "failure: "+task.getException());
+                    Toast.makeText(SignupActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                }
+            });
+        });
+    }
+}
